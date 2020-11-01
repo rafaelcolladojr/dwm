bd4a909b38acdce1340b1249c594a0c3ed031541
Rafael Collado <rafffcollado@gmail.com>
2020-11-01 07:43:53 -0500

    matching config.def.h to config.h

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@
 /* appearance */
 static unsigned int borderpx  = 3;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 80;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 80;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 80;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 80;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
@@ -71,18 +71,18 @@ static const char *browser[]  = { "qutebrowser", NULL };
  */
 ResourcePref resources[] = {
 		{ "background",         STRING,  &normbgcolor },
-		{ "background",   	STRING,  &normbordercolor },
+		{ "background",   		STRING,  &normbordercolor },
 		{ "foreground",         STRING,  &normfgcolor },
 		{ "background",         STRING,  &selbgcolor },
-		{ "color1",     	STRING,  &selbordercolor },
+		{ "color1",     		STRING,  &selbordercolor },
 		{ "foreground",         STRING,  &selfgcolor },
 		{ "borderpx",          	INTEGER, &borderpx },
-		{ "snap",      		INTEGER, &snap },
+		{ "snap",      			INTEGER, &snap },
 		{ "showbar",          	INTEGER, &showbar },
 		{ "topbar",          	INTEGER, &topbar },
 		{ "nmaster",          	INTEGER, &nmaster },
 		{ "resizehints",       	INTEGER, &resizehints },
-		{ "mfact",     	 	FLOAT,   &mfact },
+		{ "mfact",     	 		FLOAT,   &mfact },
 };
 
 static Key keys[] = {
@@ -105,6 +105,7 @@ static Key keys[] = {
 //	{ MODKEY|Mod1Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
 	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY|Mod1Mask|ShiftMask,	XK_9,	   setgaps,		   {.i = 40} },
 //	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
 //	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
 //	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
