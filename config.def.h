4a76b76e7a88f8b0e7a8fdc3555584c14a958e00
Rafael Collado <rafffcollado@gmail.com>
2020-11-09 12:03:37 -0500

    Applied Swallow patch

--- config.def.h
+++ config.def.h
@@ -1,24 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static unsigned int borderpx  = 3;        /* border pixel of windows */
-static unsigned int snap      = 32;       /* snap pixel */
+static unsigned int borderpx= 3;        /* border pixel of windows */
+static unsigned int snap = 32;       /* snap pixel */
+static const int swallowfloating = 0;		/* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 80;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 80;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 80;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 80;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
-static char normbgcolor[]       = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]       = "#bbbbbb";
-static char selfgcolor[]       = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]	= "#005577";
-static char *colors[][3]      = {
+static int showbar = 1;        /* 0 means no bar */
+static int topbar = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "San Francisco Pro Display Regular:size=14" };
+static const char dmenufont[]       = "San Francisco Pro Display Regular:size=14";
+static char normbgcolor[] = "#222222";
+static char normbordercolor[] = "#444444";
+static char normfgcolor[] = "#bbbbbb";
+static char selfgcolor[] = "#eeeeee";
+static char selbordercolor[] = "#005577";
+static char selbgcolor[] = "#005577";
+static char *colors[][3] = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
 	[SchemeSel]  = { selfgcolor, selbordercolor,  selbordercolor  },
@@ -32,9 +33,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
@@ -75,7 +78,7 @@ ResourcePref resources[] = {
 		{ "foreground",         STRING,  &normfgcolor },
 		{ "background",         STRING,  &selbgcolor },
 		{ "color1",     		STRING,  &selbordercolor },
-		{ "foreground",         STRING,  &selfgcolor },
+		{ "background",         STRING,  &selfgcolor },
 		{ "borderpx",          	INTEGER, &borderpx },
 		{ "snap",      			INTEGER, &snap },
 		{ "showbar",          	INTEGER, &showbar },
